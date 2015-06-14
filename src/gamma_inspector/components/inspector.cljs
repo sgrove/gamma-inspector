b33cc5834f9de972e00a45de7023899c110bdf34
Sean Grove <s@bushi.do>
2015-06-14 10:21:00 -0700

    Updates to the inspector component itself

--- src/gamma_inspector/components/inspector.cljs
+++ src/gamma_inspector/components/inspector.cljs
@@ -1,4 +1,4 @@
-(ns gamma-inspector.components.inspector
+(ns ^:figwheel-always gamma-inspector.components.inspector
   (:require [clojure.data :as clojure-data]
             [clojure.string :as string]
             [facebook.react.fixed-data-table]
@@ -41,9 +41,9 @@
     ;;                    true)
     :style          {:cursor "pointer"}
     :class          (cond
-                      (= row-data value) "bg-primary"
+                      (= row-data value)        "bg-primary"
                       (= row-index highlighted) "bg-info"
-                      :else nil)}
+                      :else                     nil)}
    ;; when blank put nbsp to prevent cell collapse and bad bg coloring
    (if (string/blank? cell-data) " " cell-data)))
 
@@ -78,9 +78,9 @@
                              :flexGrow       0
                              :width          50
                              :isResizable    false
-                             :dataKey        "#"
+                             :dataKey        0
                              :cellRenderer   (fn [cell-data _ _ row-index _ _]
-                                               row-index)})
+                                               cell-data)})
                  (Column #js{:label          "API"
                              :cellDataGetter getter
                              :flexGrow       0
@@ -115,17 +115,29 @@
                                                                                                                     gd/draw-elements)]
                                                                    (if driver
                                                                      (do
+                                                                       (js/console.log "RedrawSpec" (clj->js spec))
                                                                        (gd/bind driver new-program spec)
                                                                        (draw driver new-program draw-data target))
                                                                      (js/console.log "No driver!"))))} (pr-str action))
                                                  (pr-str cell-data))))})
                  (Column #js{:label          "Data"
-                             :width          20
+                             :width          100
                              :flexGrow       1
                              :isResizable    false
                              :cellDataGetter getter
                              :dataKey        3
-                             :cellRenderer   pr-str})))))))
+                             :cellRenderer   (fn [cell-data cell-data-key row-data row-index column-data width]
+                                               (let [[cmd-index api action cmd program spec target] row-data]
+                                                 (if (= action :draw)
+                                                   (str (pr-str (:draw-id spec)) " | " (pr-str (:draw-mode spec)) " | " (:count spec))
+                                                   (pr-str cell-data))))})
+                 (Column #js{:label          "Result"
+                             :width          40
+                             :flexGrow       0.25
+                             :isResizable    false
+                             :cellDataGetter getter
+                             :dataKey        4
+                             :cellRenderer   #(pr-str %)})))))))
 
 (defmulti command->string (fn [[api command spec]]
                             [api command]))
@@ -165,6 +177,10 @@
       (let [log-atom         (:log inspector-driver)
             log              (when log-atom @log-atom)
             driver           (om/get-state owner [:scratch-driver])
+            backing-driver   (:backing-driver inspector-driver)
+            backing-gl       (:gl backing-driver)
+            preview-aspect   (/ (.. backing-gl -canvas -height)
+                                (.. backing-gl -canvas -width))
             gl               (om/get-state owner [:gl])
             inspected-frame  (or (om/get-state owner [:inspected-frame]) 0)
             frame            (get-in log [:frames inspected-frame])
@@ -177,53 +193,70 @@
             content-height   350 ;;(- inspector-height header-height footer-height)
             frames-width     50
             preview-width    350
+            preview-height   (* preview-width preview-aspect)
             trace-width      (- inspector-width frames-width preview-width)
             frames           (:frames log)]
-        (dom/div #js{:className "gamma-inspector-container columnParent"
-                     :style #js {:width inspector-width}}
-                 (dom/div #js {:id "columnChild28345"
-                               :className "flexChild columnParent"}
-                          (dom/div #js{:id "columnChild87643"
-                                       :className "gamma-inspector-header flexChild"}
-                                   (dom/p #js{:style #js{}} "Gamma Inspector | Trace captured frame " inspected-frame (dom/button #js{:onClick #(om/set-state! owner [:last-rendered] (.getTime (js/Date.)))} "Refresh"))
-                                   ;; Force rerender since our log state is in an atom
-                                   ;; Will refactor out later
-                                   )
-                          (dom/div #js{:id "rowChild15388"
-                                       :className "flexChild rowParent"
-                                       :style #js{
-                                                  :height          content-height
-                                                  :overflow        "hidden"}}
-                                   (dom/div #js{:id "rowChild93604"
-                                                :className "flexChild rowParent"}
-                                            (apply dom/div #js{:className "frames"
-                                                               :style #js{:width frames-width}}
-                                                   (map (fn [id frame]
-                                                          (dom/div nil
-                                                                   (dom/a #js{:onClick (fn [event]
-                                                                                         (.stopPropagation event)
-                                                                                         (om/set-state! owner :inspected-frame id))}
-                                                                          (dom/img #js{:src (get-in frame [:screenshot :src])
-                                                                                       :href "#"
-                                                                                       :style #js{:width  50
-                                                                                                  :height 50}})
-                                                                          (dom/br nil)
-                                                                          (get-in frame [:screenshot :frame])))) (range) frames))
-                                            (om/build fixed-data-table {:items    (filter #(displayable? % (om/get-state owner :filter)) (:actions frame-trace))
-                                                                        :actions  (:actions frame-trace)
-                                                                        :get-cols pr-str
-                                                                        :height   content-height
-                                                                        :width    trace-width
-                                                                        :driver   driver})
-                                            (dom/div #js{:className "gi-trace"}
-                                                     (dom/canvas #js{:style #js{:width  preview-width
-                                                                                :height preview-width}
-                                                                     :className "checkered-bg"})))))
-                 (dom/div #js{:id        "columnChild75879"
-                              :className "flexChild"}
-                          "Filter: "(dom/label nil 
-                                               (dom/input #js{:onChange (fn [event]
-                                                                          ;;(.stopPropagation event)
-                                                                          (om/set-state! owner :filter (.. event -target -value))
-                                                                          true)
-                                                              :value    (om/get-state owner :filter)}))))))))
+        (dom/div #js{:className "debugger-container"}
+                 (dom/div #js{:className "gamma-inspector-container columnParent"
+                              :style #js {:width inspector-width}}
+                          (dom/div #js {:id "columnChild28345"
+                                        :className "flexChild columnParent"}
+                                   (dom/div #js{:id "columnChild87643"
+                                                :className "gamma-inspector-header flexChild"}
+                                            (dom/div #js{:className "panel panel-default"
+                                                         :style #js{:marginBottom 0}}
+                                                     (dom/div #js{:className "panel-heading"
+                                                                  :style #js{:marginBottom 0
+                                                                             :paddingBottom 0}} "Gamma Inspector "
+                                                                             (dom/label nil 
+                                                                                        (dom/input #js{:style #js{:border       "1px solid #ccc"
+                                                                                                                  :borderRadius 9} 
+                                                                                                       :onChange (fn [event]
+                                                                                                                   ;;(.stopPropagation event)
+                                                                                                                   (om/set-state! owner :filter (.. event -target -value))
+                                                                                                                   true)
+                                                                                                       :value    (om/get-state owner :filter)}))))
+                                            ;; Force rerender since our log state is in an atom
+                                            ;; Will refactor out later
+                                            (dom/div #js{:className "panel-body"
+                                                         :style #js{:marginTop 0
+                                                                    :padding 0}}
+                                                     (dom/div #js{:id "rowChild15388"
+                                                                  :className "flexChild rowParent"
+                                                                  :style #js{
+                                                                             :height          content-height
+                                                                             :overflow        "hidden"}}
+                                                              (dom/div #js{:id "rowChild93604"
+                                                                           :className "flexChild rowParent"}
+                                                                       (apply dom/div #js{:className "frames-inspector"
+                                                                                          :style #js{:width frames-width}}
+                                                                              (map (fn [id frame]
+                                                                                     (dom/div #js{:style #js{:position "relative"
+                                                                                                             :backgroundColor (when (= id (om/get-state owner :inspected-frame))
+                                                                                                                                "green")}}
+                                                                                              (dom/a #js{:onClick (fn [event]
+                                                                                                                    (.stopPropagation event)
+                                                                                                                    (om/set-state! owner :inspected-frame id))}
+                                                                                                     (dom/img #js{:src (get-in frame [:screenshot :src])
+                                                                                                                  :href "#"
+                                                                                                                  :style #js{:width  50
+                                                                                                                             :height 50}})
+                                                                                                     (dom/div #js{:style #js{:backgroundColor "white"
+                                                                                                                             :opacity         "0.80"
+                                                                                                                             :position        "absolute"
+                                                                                                                             :top             0
+                                                                                                                             :right           0}}
+                                                                                                              (get-in frame [:screenshot :frame]))
+                                                                                                     (dom/hr #js{:style #js{:margin "2px 0px 2px 0px"}})
+                                                                                                     ))) (range) frames))
+                                                                       (om/build fixed-data-table {:items    (filter #(displayable? % (om/get-state owner :filter)) (:actions frame-trace))
+                                                                                                   :actions  (:actions frame-trace)
+                                                                                                   :get-cols pr-str
+                                                                                                   :height   content-height
+                                                                                                   :width    trace-width
+                                                                                                   :driver   driver})
+                                                                       (dom/div #js{:className "gi-trace checkered-bg"}
+                                                                                (dom/canvas #js{:style #js{:width  preview-width
+                                                                                                           :height preview-height}}))))))
+                                   (dom/div #js{:id        "columnChild75879"
+                                                :className "flexChild"}))))))))
